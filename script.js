e29b65c83d0927a9445592920158d9d0837f8b9a
Sean Grant <pscgrant95@gmail.com>
2023-03-21 18:03:07 -0700

    created basic math functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function addFunc (num1, num2) {
+    const sumAdd = num1 + num2;
+    return sumAdd;
+};
+
+function subFunc (num1, num2) {
+    const sumSub = num1 - num2;
+    return sumSub;
+};
+
+function multiFunc (num1, num2) {
+    const sumMulti = num1 * num2;
+    return sumMulti;
+};
+
+function divFunc (num1, num2) {
+    const sumDiv = num1 / num2;
+    return sumDiv;
+}
