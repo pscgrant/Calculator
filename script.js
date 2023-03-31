eef90eec6bb59a32a0d6ba3b15048657aa5445c0
Sean Grant <pscgrant95@gmail.com>
2023-03-31 14:04:22 -0700

    Created class, button query selectors and button loops

--- script.js
+++ script.js
@@ -2,7 +2,64 @@ let num1 = '';
 let num2 = '';
 let operator = '';
 
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    };
 
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    };
+
+    delete() {
+
+    };
+
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    };
+
+    chooseOperation(operation) {
+        
+    };
+
+    compute() {
+
+    };
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand
+    };
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const equalsButton = document.querySelector('[data-equals]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+});
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+});
 
 function addFunc (num1, num2) {
     const sumAdd = num1 + num2;
