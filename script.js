7c1367fcc90e66cf151442e12e6e8a7924993fd2
Sean Grant <pscgrant95@gmail.com>
2023-03-31 16:44:10 -0700

    completed the compute func

--- script.js
+++ script.js
@@ -32,7 +32,29 @@ class Calculator {
     };
 
     compute() {
-
+        let computation
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case '*':
+                computation = prev * current;
+                break;
+            case '÷':
+                computation = prev / current;
+                break;
+            default:
+                return
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
     };
 
     updateDisplay() {
@@ -65,3 +87,12 @@ operationButtons.forEach(button => {
     })
 });
 
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
